8bd0fbb69227a112680e61788d403bd07f949833
Krystian Kalwik <ambitious156288@gmail.com>
2021-03-16 17:55:45 +0100

    subpage News

--- routes/admin.js
+++ routes/admin.js
@@ -14,17 +14,38 @@ router.all("*", (req, res, next) => {
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  // console.log(req.session.admin);
-  // const newsData = new News({
-  //   title: "title",
-  //   description: "description",
-  // });
-  // newsData.save((err) => console.log(err));
-  res.render("admin/index", { title: "Admin" });
+  News.find({}, (err, data) => {
+    console.log(data);
+    res.render("admin/index", { title: "Admin", data });
+  });
 });
 
 router.get("/news/add", function (req, res, next) {
-  res.render("admin/news-form", { title: "Add news" });
+  res.render("admin/news-form", { title: "Add news", body: {}, errors: {} });
+});
+
+router.post("/news/add", function (req, res, next) {
+  const body = req.body;
+
+  const newsData = new News(body);
+
+  const errors = newsData.validateSync();
+  console.log(errors);
+
+  newsData.save((err) => {
+    if (err) {
+      res.render("admin/news-form", { title: "Add news", errors, body });
+      return;
+    }
+
+    res.redirect("/admin");
+  });
+});
+
+router.get("/news/delete/:id", function (req, res, next) {
+  News.findByIdAndDelete(req.params.id, (err) => {
+    res.redirect("/admin");
+  });
 });
 
 module.exports = router;
--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var router = express.Router();
+const News = require("../models/news");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("news", { title: "News" });
+router.get("/", (req, res) => {
+  const search = req.query.search;
+
+  const findeNews = News.find({ title: search }).sort({
+    created: -1,
+  });
+
+  findeNews.exec((err, data) => {
+    res.render("news", { title: "News", data });
+  });
 });
 
 module.exports = router;
