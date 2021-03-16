94dd4364e099a0a98276897cfcc4c0301770f55e
Krystian Kalwik <ambitious156288@gmail.com>
2021-03-16 21:10:33 +0100

    app refactoring

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var createError = require("http-errors");
-var cookieSession = require("cookie-session");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var config = require("./config");
+const createError = require("http-errors");
+const cookieSession = require("cookie-session");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const config = require("./config");
 const mongoose = require("mongoose");
 
 mongoose.connect(config.db, {
@@ -18,14 +18,13 @@ db.once("open", function () {
   console.log("were connected!");
 });
 
-var indexRouter = require("./routes/index");
-var newsRouter = require("./routes/news");
-var quizRouter = require("./routes/quiz");
-var adminRouter = require("./routes/admin");
+const indexRouter = require("./routes/index");
+const newsRouter = require("./routes/news");
+const quizRouter = require("./routes/quiz");
+const adminRouter = require("./routes/admin");
 
-var app = express();
+const app = express();
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
@@ -43,10 +42,6 @@ app.use(
   })
 );
 
-// app.use(function (req, res, next) {
-//   req.sessionOptions.maxAge = req.session.maxAge || req.sessionOptions.maxAge;
-// });
-
 app.use(function (req, res, next) {
   res.locals.path = req.path;
 
@@ -58,18 +53,14 @@ app.use("/news", newsRouter);
 app.use("/quiz", quizRouter);
 app.use("/admin", adminRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function (next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   db:
     "mongodb+srv://admin:admin@cluster0.jrtx3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   keySessions: ["MyKey"],
-  maxAgeSession: 24 * 60 * 60 * 1000, // 24 hours
+  maxAgeSession: 24 * 60 * 60 * 1000,
 };
